49a8230d954efa1276d2d54b867473c43f62d4f6
Luiz Gasparetto <luizegasp@gmail.com>
2022-10-12 03:34:15 -0300

    chore: add swagger to app

--- src/core/infra/http/app.ts
+++ src/core/infra/http/app.ts
@@ -7,10 +7,15 @@ import express from "express";
 
 import { router } from './routes';
 
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "../../../../docs/swagger.json";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
